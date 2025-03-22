a1da9fb8d703f14e38597210e64b9db842dd7956
alessitomas <tomasalessi@gmail.com>
2025-03-22 18:05:31 -0300

    Request structure improved

--- etl/bin/main.ml
+++ etl/bin/main.ml
@@ -1,6 +1,5 @@
 open Etl_lib.Request
 
 let () = Printf.printf "%s\n" "Hello, world!" ;
-        
         print_endline ("Received body\n" ^ order_data()) ;
         print_endline ("Received body\n" ^ order_item_data()) ;;
--- etl/lib/request.ml
+++ etl/lib/request.ml
@@ -1,16 +1,15 @@
 open Lwt
 open Cohttp_lwt_unix
 
+let api_url = "http://0.0.0.0:8000"
 
 let get_order =
-  Client.get (Uri.of_string "http://44.220.173.123:8000/order") >>= fun (_, body) ->
-  body |> Cohttp_lwt.Body.to_string >|= fun body ->
-  body ;;
+  Client.get (Uri.of_string (api_url ^ "/order") ) >>= fun (_, body) ->
+  body |> Cohttp_lwt.Body.to_string ;;
 
 let get_order_item =
-  Client.get (Uri.of_string "http://44.220.173.123:8000/orderItem") >>= fun (_, body) ->
-  body |> Cohttp_lwt.Body.to_string >|= fun body ->
-  body ;;
+  Client.get (Uri.of_string (api_url ^ "/orderItem")) >>= fun (_, body) ->
+  body |> Cohttp_lwt.Body.to_string ;;
 
 let order_data () = Lwt_main.run get_order ;;
 let order_item_data () = Lwt_main.run get_order_item ;;
